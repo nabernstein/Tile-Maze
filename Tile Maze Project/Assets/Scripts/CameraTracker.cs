78c5ad8dd0ef0bd65048a9335a4e517d51106690
Nick Bernstein <skolyr@csu.fullerton.edu>
2018-06-24 19:11:45 -0700

    Changed Camera to horizontal follow
    No longer follows character if player moves vertically.

--- Tile Maze Project/Assets/Scripts/CameraTracker.cs
+++ Tile Maze Project/Assets/Scripts/CameraTracker.cs
@@ -6,16 +6,19 @@ public class CameraTracker : MonoBehaviour {
 
     public GameObject player;       //Public variable to store a reference to the player game object
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private Vector3 startPos;
 
     // Use this for initialization
     void Start() {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
+        startPos = transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate() {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
+        transform.position = new Vector3(player.transform.position.x, startPos.y, startPos.z);
     }
 }
